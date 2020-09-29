0d2a44e34847874dd983411ee659c68657b5b61d
Dan <dan.mok999@yandex.ru>
2020-09-29 17:37:50 +0500

    Task 1-2 done

--- index.js
+++ index.js
@@ -16,10 +16,26 @@ function processCommand(command) {
         case 'exit':
             process.exit(0);
             break;
+        case 'show':
+            for (const todo of getTodos())
+                console.log(todo);
+            break;
         default:
             console.log('wrong command');
             break;
     }
+}
+
+function getTodos() {
+    return files
+        .map(file => file.match(/\/\/.*todo.*/gi))
+        .flat(Infinity)
+        .filter(x => x)
+        .sort((a, b) => countItem(b, '!') - countItem(a, '!'));
+}
+
+function countItem(string, item){
+    return string.split('').reduce((p, i) => i === item ? p + 1 : p, 0);
 }
 
 // TODO you can do it!
